0aff01f2ff2644ca0de0388885235ea88d44f3a3
2048khz-gachi-rmx <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2026-09-22 18:34:34 +0700

    https://github.com/rdp/ffmpeg-windows-build-helpers/issues/783

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -1387,7 +1387,7 @@ build_lame() {
   do_svn_checkout https://svn.code.sf.net/p/lame/svn/trunk/lame lame_svn 
   cd lame_svn
     sed -i.bak '1s/^\xEF\xBB\xBF//' libmp3lame/i386/nasm.h # Remove a UTF-8 BOM that breaks nasm if it's still there; should be fixed in trunk eventually https://sourceforge.net/p/lame/patches/81/
-    generic_configure "--enable-nasm --enable-libmpg123"
+    generic_configure "--enable-nasm --enable-libmpg123 --disable-hardening"
     do_make_and_make_install
   cd ..
 }
